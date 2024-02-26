55158b0cd5f8b31b419ab73c70899fd3568c988f
Timothy Machnacki <tmachnacki@signs365.com>
2024-02-26 11:41:12 -0500

    init files and config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<div>My App</div>} />
+    </Routes>
+  )
+}
+
+export { App };
